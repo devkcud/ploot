e8bf1358a35ff100ce71b5a1ccfcdf04f9ae7d16
devkcud <patommmmm@proton.me>
2024-03-16 21:32:10 -0300

    feat: Add simple routing

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,17 @@
-use ploot::server;
+use ploot::{response::ReponseConstructor, server};
+use std::{
+    io::{Result, Write},
+    net::TcpStream,
+};
+
+fn home(mut stream: TcpStream) -> Result<()> {
+    stream.write_all(&ReponseConstructor::new().set_content("Hi").build())
+}
 
 fn main() {
-    let server = server::Listener::new();
+    let mut server = server::Listener::new();
+
+    server.GET("/", home);
 
     match server.listen(":1234") {
         Ok(_) => (),
--- src/server.rs
+++ src/server.rs
@@ -1,16 +1,23 @@
 use crate::response::ReponseConstructor;
 use std::{
-    io::{Error, ErrorKind, Result, Write},
+    collections::HashMap,
+    io::{BufRead, BufReader, Error, ErrorKind, Result, Write},
     net::{TcpListener, TcpStream},
 };
 
 const DEFAULT_HOSTNAME: &str = "127.0.0.1";
 
-pub struct Listener;
+type MethodHandle = fn(stream: TcpStream) -> Result<()>;
+
+pub struct Listener {
+    routes: HashMap<&'static str, Route>,
+}
 
 impl Listener {
     pub fn new() -> Self {
-        Self
+        Self {
+            routes: HashMap::new(),
+        }
     }
 
     pub fn listen(&self, host: &str) -> Result<()> {
@@ -23,27 +30,105 @@ impl Listener {
 
         let listener = TcpListener::bind(format!("{ip}:{port}"))?;
 
+        let mut count = 0;
+
+        for route in &self.routes {
+            count += route.1.methods.len();
+        }
+
+        println!("Loaded {} methods in {} routes", count, self.routes.len());
         println!(
             "Listening at http://{}",
             listener.local_addr().unwrap().to_string()
         );
 
         for stream in listener.incoming() {
-            match stream {
-                Ok(o) => Self::handle_request(o),
-                Err(e) => {
-                    return Err(e);
-                }
-            }
+            self.handle_request(stream.unwrap());
         }
 
         Ok(())
     }
 
-    fn handle_request(mut stream: TcpStream) {
-        stream
-            .write_all(&ReponseConstructor::new().set_content("Hi").build())
-            .unwrap();
+    #[allow(non_snake_case)]
+    pub fn GET(&mut self, path: &'static str, handle: MethodHandle) -> () {
+        self.route("GET", path, handle);
+    }
+
+    #[allow(non_snake_case)]
+    pub fn POST(&mut self, path: &'static str, handle: MethodHandle) -> () {
+        self.route("POST", path, handle);
+    }
+
+    #[allow(non_snake_case)]
+    pub fn PUT(&mut self, path: &'static str, handle: MethodHandle) -> () {
+        self.route("PUT", path, handle);
+    }
+
+    #[allow(non_snake_case)]
+    pub fn PATCH(&mut self, path: &'static str, handle: MethodHandle) -> () {
+        self.route("PATCH", path, handle);
+    }
+
+    #[allow(non_snake_case)]
+    pub fn DELETE(&mut self, path: &'static str, handle: MethodHandle) -> () {
+        self.route("DELETE", path, handle);
+    }
+
+    fn route(&mut self, method: &'static str, path: &'static str, handle: MethodHandle) -> () {
+        self.routes
+            .entry(path)
+            .or_insert(Route {
+                methods: HashMap::new(),
+            })
+            .methods
+            .insert(method, handle);
+    }
+
+    fn handle_request(&self, mut stream: TcpStream) -> () {
+        let buf_reader = BufReader::new(&mut stream);
+        let http_request = buf_reader
+            .lines()
+            .map(|result| result.unwrap())
+            .take_while(|line| !line.is_empty())
+            .collect::<Vec<String>>();
+
+        let status_line = http_request
+            .get(0)
+            .unwrap()
+            .split_whitespace()
+            .take(2)
+            .collect::<Vec<&str>>();
+
+        let path = *status_line.get(1).unwrap();
+        let method = *status_line.get(0).unwrap();
+
+        if self.routes.get(path).is_none() {
+            stream
+                .write_all(
+                    &ReponseConstructor::new()
+                        .set_status("404 Not Found")
+                        .set_content_type("application/json")
+                        .set_content(r#"{ "error": "Not Found" }"#)
+                        .build(),
+                )
+                .unwrap();
+            return;
+        }
+
+        if self.routes[path].methods.get(method).is_none() {
+            stream
+                .write_all(
+                    &ReponseConstructor::new()
+                        .set_status("405 Method Not Allowed")
+                        .set_content_type("application/json")
+                        .set_content(r#"{ "error": "Method Not Allowed" }"#)
+                        .build(),
+                )
+                .unwrap();
+            return;
+        }
+
+        self.routes[path].methods[method](stream).unwrap();
     }
 
     fn parse_host(host: &str) -> Result<(String, u16)> {
@@ -68,4 +153,8 @@ impl Listener {
 
         Ok((ip, port))
     }
+}
+
+struct Route {
+    methods: HashMap<&'static str, MethodHandle>,
 }
